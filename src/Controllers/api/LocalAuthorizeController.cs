2d1d6230b48346201117d47741be5f4504cf688e
bwalk2895 <bob.walker@codeaperture.io>
2016-08-02 20:37:11 -0500

    Adding Another Bad Example

---
+++ src/Controllers/api/LocalAuthorizeController.cs
@@ -0,0 +1,40 @@
+﻿using CodeAperture.IO.HDC2016.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeAperture.IO.HDC2016.Controllers.api
+{
+    public class LocalAuthorizeController : Controller
+    {
+        private ILocalScopeExampleDataService _dataService;
+
+        public LocalAuthorizeController(ILocalScopeExampleDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [Route("api/LocalAuthorize")]
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetItems(int parentId)
+        {
+            var results = _dataService.GetList(parentId);
+
+            return new ObjectResult(results);
+        }
+
+        [Route("api/LocalAuthorize/{itemId}")]
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetItemById(int itemId)
+        {
+            var results = _dataService.GetItem(itemId);
+
+            return new ObjectResult(results);
+        }
+    }
+}
