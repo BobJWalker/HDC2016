fbbd7e900214fd2756bf85291c3ff970b84213d5
bwalk2895 <bob.walker@codeaperture.io>
2016-08-11 19:37:43 -0500

    Creating non-core application for initial samples

---
+++ src/HDCSample/SampleSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleSite.Startup))]
+namespace SampleSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
