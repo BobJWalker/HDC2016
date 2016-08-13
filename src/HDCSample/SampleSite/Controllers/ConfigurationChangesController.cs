0acbaf46f0947ae0d54d1bf9c9ebde6379dd1c95
bwalk2895 <bob.walker@codeaperture.io>
2016-08-13 14:57:13 -0500

    Adding in Powerpoint Presentation getting more scaffolding added

---
+++ src/HDCSample/SampleSite/Controllers/ConfigurationChangesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeAperture.HDC2016.SampleSite.Controllers
+{
+    public class ConfigurationChangesController : Controller
+    {
+        // GET: 
+        public ActionResult ContentSecurityPolicy()
+        {
+            return View();
+        }
+
+        public ActionResult CookiePolicies()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/HDCSample/SampleSite/Controllers/FilterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeAperture.HDC2016.SampleSite.Controllers
+{
+    public class FilterController : Controller
+    {
+        public ActionResult GlobalExceptionHandling()
+        {
+            return View();
+        }
+
+        public ActionResult ActionFilter()
+        {
+            return View();
+        }
+    }
+}
